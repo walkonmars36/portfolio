2da8af305e688abd5ddd30ac42cf3be058dd5438
walkonmars36 <mark@walkonmars.co.uk>
2021-05-19 19:23:06 +0100

    small hover change

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
+// Nav overlay
+
 const navToggle = document.querySelector(".menu-button");
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
 
 navToggle.addEventListener("click", (_) => {
   document.body.classList.toggle("nav-is-open");
 });
 
-nav.addEventListener('click', _ => {
-  document.body.classList.remove('nav-is-open')
-})
+nav.addEventListener("click", (_) => {
+  document.body.classList.remove("nav-is-open");
+});
 
+// Projects modal
 
+const portfolio = document.querySelector(".projects");
 
-const portfolio = document.querySelector('.projects')
-
-portfolio.addEventListener('click', e => {
+portfolio.addEventListener("click", (e) => {
   //console.log(e)
-  e.preventDefault()
+  e.preventDefault();
 
-  const modalToggle = e.target.closest('.projects__link')
+  const modalToggle = e.target.closest(".projects__link");
   //console.log(modalToggle)
-  if (! modalToggle) return
+  if (!modalToggle) return;
 
-  const modal = modalToggle.parentNode.nextElementSibling
-  const closeButton = modal.querySelector('.modal-button')
+  const modal = modalToggle.parentNode.nextElementSibling;
+  const closeButton = modal.querySelector(".modal-button");
 
-  modal.classList.add('is-open')
+  modal.classList.add("is-open");
 
-  closeButton.addEventListener('click', _ => {
-    modal.classList.remove('is-open')
-  })
-
-
-})
+  closeButton.addEventListener("click", (_) => {
+    modal.classList.remove("is-open");
+  });
+});
