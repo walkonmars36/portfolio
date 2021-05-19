8ef365ac5ffcd0207bbf38d9307ebd7f472bd6e1
walkonmars36 <mark@walkonmars.co.uk>
2021-05-19 19:07:41 +0100

    finishing up styling

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-const navToggle = document.querySelector(".menu__button");
+const navToggle = document.querySelector(".menu-button");
+const nav = document.querySelector('nav')
 
 navToggle.addEventListener("click", (_) => {
   document.body.classList.toggle("nav-is-open");
 });
+
+nav.addEventListener('click', _ => {
+  document.body.classList.remove('nav-is-open')
+})
+
+
+
+const portfolio = document.querySelector('.projects')
+
+portfolio.addEventListener('click', e => {
+  //console.log(e)
+  e.preventDefault()
+
+  const modalToggle = e.target.closest('.projects__link')
+  //console.log(modalToggle)
+  if (! modalToggle) return
+
+  const modal = modalToggle.parentNode.nextElementSibling
+  const closeButton = modal.querySelector('.modal-button')
+
+  modal.classList.add('is-open')
+
+  closeButton.addEventListener('click', _ => {
+    modal.classList.remove('is-open')
+  })
+
+
+})
